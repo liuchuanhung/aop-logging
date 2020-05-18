65601f18495e3110068fa3771d9e6566caf6ad87
Ryan Liu <ryan.liu@slalom.com>
2020-05-18 09:29:40 -0500

    created a dummy aspect annotation that does some basic logging with annotated on a class method.

--- src/main/java/org/liuchuanhung/aop/logging/aspect/LoggableAspect.java
+++ src/main/java/org/liuchuanhung/aop/logging/aspect/LoggableAspect.java
@@ -3,20 +3,27 @@ package org.liuchuanhung.aop.logging.aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
 public class LoggableAspect {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggableAspect.class);
+
     @Around("@annotation(org.liuchuanhung.aop.logging.annotation.Loggable)")
-    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+    public Object loggable(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         Object proceed = joinPoint.proceed();
         long executionTime = System.currentTimeMillis() - start;
-
-        System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
-
+        LOGGER.info("Execution Time: " + executionTime + "ms");
+        LOGGER.info("Calling Class: " + joinPoint.getStaticPart().getSignature().getDeclaringTypeName()); //calling class
+        LOGGER.info("Calling Method: " + joinPoint.getStaticPart().getSignature().getName()); //calling method
+        LOGGER.info("Arguments Passed: " + Arrays.toString(joinPoint.getArgs()));
         return proceed;
     }
 }
--- src/main/java/org/liuchuanhung/aop/logging/controller/DummyController.java
+++ src/main/java/org/liuchuanhung/aop/logging/controller/DummyController.java
@@ -1,22 +1,25 @@
 package org.liuchuanhung.aop.logging.controller;
 
 import org.liuchuanhung.aop.logging.annotation.Loggable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/dummy")
 public class DummyController {
 
     @Loggable
-    @GetMapping
-    public String greeting(@RequestParam("name") String name) {
+    @GetMapping("/{dummyPath}")
+    public String greeting(@RequestParam("name") String name , @PathVariable("dummyPath") String dummyPath) {
         StringBuilder sb = new StringBuilder();
         sb.append("Hello ");
         sb.append(name);
         sb.append("!");
         return sb.toString();
+    }
+
+    @Loggable
+    @PostMapping
+    public String greeting2(@RequestBody String request) {
+        return "nice to meet you.";
     }
 }
